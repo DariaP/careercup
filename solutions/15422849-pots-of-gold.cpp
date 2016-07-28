0bb69a74e48c6b784a963de5f8dabacb37250543
Daria <dashka.kh@gmail.com>
2016-07-28 12:09:26 -0700

    Save partial results

--- solutions/15422849-pots-of-gold.cpp
+++ solutions/15422849-pots-of-gold.cpp
@@ -16,45 +16,83 @@ knowing that B is playing optimally as well.
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <unordered_map>
+#include <unordered_set>
 #define BOOST_TEST_MODULE PotsOfGoldTests
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
-class Choice {
+class Move {
 public:
   int idx, sum;
-  Choice(int i, int s) {
+  Move() { idx = -1; }
+  Move(int i, int s) {
     idx = i; sum = s;
   }
+  bool valid() {
+    return idx != -1;
+  }
 };
-int pickMin(std::vector<int> pots, int startIdx, int endIdx);
-Choice pickMax(std::vector<int> pots, int startIdx, int endIdx);
 
-int pickMin(std::vector<int> pots, int startIdx, int endIdx) {
+class Moves {
+  std::vector<std::vector<Move>> moves;
+
+public:
+  Moves(int size): moves(size) {}
+
+  void save(int start, int end, Move move) {
+    if (moves[start].size() == 0) {
+      moves[start] = std::vector<Move>(moves.size());
+    }
+    moves[start][end] = move;
+  }
+
+  bool has(int start, int end) {
+    return moves[start].size() != 0 && moves[start][end].valid();
+  }
+
+  Move get(int start, int end) {
+    return moves[start][end];
+  }
+};
+
+int pickMin(std::vector<int> pots, int startIdx, int endIdx, Moves &moves);
+Move pickMax(std::vector<int> pots, int startIdx, int endIdx, Moves &moves);
+
+int pickMin(std::vector<int> pots, int startIdx, int endIdx, Moves &moves) {
+
   if (startIdx == endIdx) {
     return 0;
   }
 
-  Choice pickFront = pickMax(pots, startIdx + 1, endIdx),
-         pickEnd = pickMax(pots, startIdx, endIdx - 1);
+  Move pickFront = pickMax(pots, startIdx + 1, endIdx, moves),
+         pickEnd = pickMax(pots, startIdx, endIdx - 1, moves);
 
   return (pickFront.sum < pickEnd.sum) ? pickFront.sum : pickEnd.sum;
 }
 
-Choice pickMax(std::vector<int> pots, int startIdx, int endIdx) {
+Move pickMax(std::vector<int> pots, int startIdx, int endIdx, Moves &moves) {
+
+  if (moves.has(startIdx, endIdx)) {
+    return moves.get(startIdx, endIdx);
+  }
+
   if (startIdx == endIdx) {
-    return Choice(startIdx, pots[startIdx]);
+    Move lastMove = Move(startIdx, pots[startIdx]);
+    moves.save(startIdx, endIdx, lastMove);
+    return lastMove;
   }
 
-  int pickFrontSum = pots[startIdx] + pickMin(pots, startIdx + 1, endIdx),
-      pickEndSum = pots[endIdx] + pickMin(pots, startIdx, endIdx - 1);
+  int pickFrontSum = pots[startIdx] + pickMin(pots, startIdx + 1, endIdx, moves),
+      pickEndSum = pots[endIdx] + pickMin(pots, startIdx, endIdx - 1, moves);
 
-  if ( pickFrontSum > pickEndSum) {
-    return Choice(startIdx, pickFrontSum);
-  } else {
-    return Choice(endIdx, pickEndSum);
-  }
+  Move bestMove = ( pickFrontSum > pickEndSum) ?
+    Move(startIdx, pickFrontSum) :
+    Move(endIdx, pickEndSum);
+
+  moves.save(startIdx, endIdx, bestMove);
+  return bestMove;
 }
 
 enum Player {
@@ -69,13 +107,28 @@ void switchPlayer(Player &player) {
   }
 }
 
+Moves calcMoves(std::vector<int> pots) {
+  Moves moves(pots.size());
+  if (pots.size() > 0) {
+    pickMax(pots, 0, pots.size() - 1, moves);
+  }
+  if (pots.size() > 1) {
+    pickMax(pots, 1, pots.size() - 1, moves);
+    pickMax(pots, 0, pots.size() - 2, moves);
+  }
+  return moves;
+}
+
 std::vector<int> play(std::vector<int> pots) {
+
+  Moves allAMoves = calcMoves(pots);
+
   std::vector<int> aMoves;
   int start = 0, end = pots.size() - 1;
   Player player = Player::A;
 
   while (start <= end) {
-    int move = pickMax(pots, start, end).idx;
+    int move = allAMoves.get(start, end).idx;
     if (player == Player::A) {
       aMoves.push_back(move);
     }
