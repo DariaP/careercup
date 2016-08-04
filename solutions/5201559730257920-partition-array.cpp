5dc748181e1dea53d015832c48ee7b043a1177ff
Daria <dashka.kh@gmail.com>
2016-08-03 20:35:47 -0700

    Edited problem text

--- solutions/5201559730257920-partition-array.cpp
+++ solutions/5201559730257920-partition-array.cpp
@@ -1,5 +1,8 @@
 /*
-Give you an array which has n integers,it has both positive and negative integers.Now you need sort this array in a special way.After that,the negative integers should in the front,and the positive integers should in the back.Also the relative position should not be changed.
+Give you an array which has n integers,it has both positive and negative integers.
+Now you need sort this array in a special way.
+After that,the negative integers should in the front,and the positive integers should in the back.
+Also the relative position should not be changed.
 eg. -1 1 3 -2 2 ans: -1 -2 1 3 2.
 o(n)time complexity and o(1) space complexity is perfect.
 */
